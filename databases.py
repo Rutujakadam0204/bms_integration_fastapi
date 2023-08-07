cc8db02c87ce465d441934947e40fec1d7f7dcd4
rutuja.kadam <rutujakadam0204@gmail.com>
2023-08-07 15:00:22 +0530

    2 db url change commit

--- databases.py
+++ databases.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # DB_URL = 'mysql://root:Temp123@127.0.0.1:3306/rk_company'
-DB_URL = 'postgres://rk_company_user:t5BT2Qlfwhbs1Qp0IFAY9Zly4nRaOYXr@dpg-cj8b6ekl975s738qjob0-a/rk_company'
+DB_URL = 'postgresql://rk_company_user:t5BT2Qlfwhbs1Qp0IFAY9Zly4nRaOYXr@dpg-cj8b6ekl975s738qjob0-a/rk_company'
 engine = create_engine(DB_URL)
 
 Base = declarative_base()
