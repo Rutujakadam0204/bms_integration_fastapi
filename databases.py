8d42456f7ce0752e121ad83c1289958d197a1f82
rutuja.kadam <rutujakadam0204@gmail.com>
2023-08-08 13:28:40 +0530

    project db url change

--- databases.py
+++ databases.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import sessionmaker
 """The declarative_base is used to create base classes for declarative SQLAlchemy models."""
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = 'mysql://root:Temp123@127.0.0.1:3306/rk_company'
-# DB_URL = 'postgresql://rk_company_user:t5BT2Qlfwhbs1Qp0IFAY9Zly4nRaOYXr@dpg-cj8b6ekl975s738qjob0-a/rk_company'
+# DB_URL = 'mysql://root:Temp123@127.0.0.1:3306/rk_company'
+DB_URL = 'postgresql://rk_company_user:t5BT2Qlfwhbs1Qp0IFAY9Zly4nRaOYXr@dpg-cj8b6ekl975s738qjob0-a/rk_company'
 engine = create_engine(DB_URL)
 
 Base = declarative_base()
